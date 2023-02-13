4125c3945b768edea881135bfc766fe54a360992
Benjamin <benjamin.guy.thomas@gmail.com>
2023-02-13 19:27:25 +0100

    Play with structs

--- cpp/00-hello-raylib/main.cpp
+++ cpp/00-hello-raylib/main.cpp
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <iostream>
 
 #define SCREEN_WIDTH 600
 #define SCREEN_HEIGHT 600
@@ -7,13 +8,19 @@
 int main() {
     // Init
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE);
+    Vector2 pos{0, 0};
 
     // Game loop
     while (!WindowShouldClose()) {
         // Update
+        pos.x += 1;
+        pos.y += 2;
 
         // Render
         BeginDrawing();
+        if ((int) pos.x % 1000 == 0) {
+            std::cout << "X=" << pos.x << "\t" "Y=" << pos.y << std::endl;
+        }
         ClearBackground(ORANGE);
         DrawRectangle(10, 10, 100, 100, RED);
         EndDrawing();
